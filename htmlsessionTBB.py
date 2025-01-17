9bf50fd29dc3e4b22fccb3ae2ccba8762c5aaeb1
Ramon Castillo <ramon@rcastillo.net>
2025-01-17 21:18:06 +0100

    added some debug improvements to hunt down the 404 error

--- htmlsessionTBB.py
+++ htmlsessionTBB.py
@@ -9,42 +9,45 @@ DEBUG=True
 #url='https://travelbybaking.com'
 #url='https://travelbybaking.com/sweet-recipes/'
 #url='https://travelbybaking.com/almond-cookies/'
-url='https://travelbybaking.com/apple-mousse-cake-with-greek-yoghurt/'
+#url='https://travelbybaking.com/apple-mousse-cake-with-greek-yoghurt/'
+def wLine(row):
+    myRow=str(row)
+    with open("localworking/renderedtosoupurl.txt",'a') as L:
+        L.write(myRow)
 def h_name(url):
     s_url=url.split('/')
     hname=s_url[len(s_url)-2]
     return hname
 
-#with open('link_test.txt','r') as Recipes_links:
-    #for Link in Recipes_links:
-session = HTMLSession()    
-r=session.get(url)
-#about = r.html.find('title,h3,p,li')
-r.html.render()
-#Rendered=r.html.html
-#about = r.html.find('title, h3,p,li')
-#
-# BeatifulSoup
-#
-soup=BeautifulSoup(r.html.html,'html.parser')
-#print(dir(html))
-#html.render(scrolldown=True, sleep=1, keep_page=True)
-#H=r.content
-#content=soup.select('title,h3,p,li,lo')
-content=soup.find_all(["title","h3","p","li","lo","ytp-title-link yt-uix-sessionlink"])
-
-
-hname=h_name(url)
-
-
-o="localworking/"+h_name(url)+"_soup.html"
-for item in content:
-    print(item)
-    Line=str(item)+"\n"
-    with open("localworking/renderedtosoupurl.txt",'a') as L:
-        L.write(Line)
-    
-if DEBUG: print ("Creating file =>",o)
-new_html=soup.prettify()
-with open(o,'w') as H:
-      H.write(new_html)
+with open('link_test.txt','r') as Recipes_links:
+    for url in Recipes_links:
+        if not url.startswith("#"):
+            if DEBUG: print("Making session with",url)
+            session = HTMLSession() 
+            r=session.get(url)
+            if r.status_code == 404:
+                WA="<ERROR 404> "+url+"</ERROR 404>"
+                print(WA)
+                wLine(WA)
+                continue
+            r.html.render()
+            #
+            # # # BeatifulSoup
+            # # 
+            # # #
+            # 
+            soup=BeautifulSoup(r.html.html,'html.parser')
+            content=soup.find_all(["title","h3","p","li","lo","ytp-title-link yt-uix-sessionlink"])
+            hname=h_name(url)
+            Header="<Header>"+str(hname)+"</Header>\n"
+            wLine(Header)
+            for item in content:
+                if DEBUG: print(item)
+                Line=str(item)+"\n"
+                wLine(Line)
+            #writting a html backup per page    
+            new_html=soup.prettify()
+            o="localworking/"+h_name(url)+"_soup.html"
+            if DEBUG: print ("Creating file =>",o)
+            with open(o,'w') as H:
+                H.write(new_html)
