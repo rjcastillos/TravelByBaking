6ad26752d5ed486c830d52ba280410be950d0293
Lenovo User Station <ramonjoseph.c@gmail.com>
2025-01-23 20:15:35 +0100

    html2json now support multi urls from file

--- html2json.py
+++ html2json.py
@@ -7,13 +7,12 @@
 # pattern2=m1+"(.*?)"+m2
 # https://docs.python.org/3/library/re.html
 from requests_html import HTMLSession
-from bs4 import BeautifulSoup
 # requests-html 2.32.3
 #https://requests.readthedocs.io/projects/requests-html/en/latest/
-import re
+#
 import json
 DEBUG=False
-INFILE="localworking/prueba.txt"
+LINKSTOPROCESS="links.txt"
 OUTPUTFILE="allRecipesData.json"
 TAGS_TO_PROCESS=("Link","Header","title","p","li","h3")
 CLASSES_TO_PROCESS=("wp-block-heading","has-text-align-center wp-block-post-title","has-text-align-center wp-block-post-title")
@@ -37,93 +36,104 @@ def h_name(url):
     s_url=url.split('/')
     hname=s_url[len(s_url)-2]
     return hname
-def jSonPrint(Myinfo):
-    allInfo[Myinfo['RecipeCodeName']]=Myinfo
-    fData=json.dumps(Myinfo,indent=4)
+def jSonPrint(ThisInfo):
+    Header=ThisInfo['RecipeCodeName']
+    allInfo[Header]=ThisInfo
+    fData=json.dumps(ThisInfo,indent=4)
     print("....json....")
     print(fData)
 
+with open(LINKSTOPROCESS,'r') as Recipes_links:
+    for _line in Recipes_links:
+        if not _line.startswith("#"):
+            url=_line.replace("\n","")
+            ##### Getting the Header / RecipeCodeName
+            # Always printed 
+            #
+            Header=h_name(url)
+            jLine='"RecipeCodeName":'+'"'+Header+'"'
+            Myinfo['RecipeCodeName']=Header
+            mPrint(jLine)
 
-##### Getting the Header / RecipeCodeName
-# Always printed 
-#
-Header=h_name(url)
-jLine='"RecipeCodeName":"'+'"'+Header+'"'
-Myinfo['RecipeCodeName']=Header
-mPrint(jLine)
+            ##### Getting the Session and Rendering the page
+            s = HTMLSession()
+            r = s.get(url)
 
-##### Getting the Session and Rendering the page
-s = HTMLSession()
-r = s.get(url)
+            r.html.render(sleep=1)
 
-r.html.render(sleep=1)
+            if DEBUG:print(r.status_code)
 
-if DEBUG:print(r.status_code)
+            #ShortDescription=<title>
+            ShortDescription=r.html.find("title" , first=True)
+            jLine='"ShortDescription :"'+ShortDescription.text
+            Myinfo['ShortDescription']=ShortDescription.text
+            if DEBUG : mPrint(jLine)
 
-#ShortDescription=<title>
-ShortDescription=r.html.find("title" , first=True)
-jLine='"ShortDescription :"'+ShortDescription.text
-Myinfo['ShortDescription']=ShortDescription.text
-if DEBUG : mPrint(jLine)
+            #Title 
+            Title=r.html.xpath('/html/body/div/main/div[1]/h3',first=True)
+            Myinfo['Title']=Title.text
 
-#Title 
-Title=r.html.xpath('/html/body/div/main/div[1]/h3',first=True)
-Myinfo['Title']=Title.text
+            #Descrpition
+            Description=r.html.xpath('/html/body/div/main/div[2]/p[1]',first=True)
+            try:
+                Myinfo['Description']=Description.text
+            except:
+                Myinfo['Description']="CHECK DESCRIPTION @"+url
 
-#Descrpition
-Description=r.html.xpath('/html/body/div/main/div[2]/p[1]',first=True)
-Myinfo['Description']=Description.text
+            #Ingredients as a list
+            Myinfo['Ingredients']=[]
+            xIngredients=r.html.xpath('/html/body/div/main/div[2]/div/div[2]/ul')
+            for ulli in xIngredients:
+                if DEBUG: print(ulli.text)
+                Myinfo['Ingredients'].append(ulli.text)   
 
-#Ingredients as a list
-Myinfo['Ingredients']=[]
-xIngredients=r.html.xpath('/html/body/div/main/div[2]/div/div[2]/ul')
-for ulli in xIngredients:
-     if DEBUG: print(ulli.text)
-     Myinfo['Ingredients'].append(ulli.text)   
+            #Directions as a list
+            Myinfo['Directions']=[]
+            xDirections=r.html.xpath('/html/body/div/main/div[2]/div/div[3]')
 
-#Directions as a list
-Myinfo['Directions']=[]
-xDirections=r.html.xpath('/html/body/div/main/div[2]/div/div[3]')
+            if DEBUG: print("Directions type ",type(xDirections),"and length",len(xDirections))
+            for li in xDirections:
+                if DEBUG: print(li.text)
+                Myinfo['Directions'].append(li.text)
 
-if DEBUG: print("Directions type ",type(xDirections),"and length",len(xDirections))
-for li in xDirections:
-    if DEBUG: print(li.text)
-    Myinfo['Directions'].append(li.text)
+            # Serving Size as a list
+            Myinfo['ServingSize']={}
 
-# Serving Size as a list
-Myinfo['ServingSize']={}
+            xServingSize=r.html.xpath('/html/body/div/main/div[2]/div/div[1]/div[1]')
+            #print("xServingSize : len type and content ",len(xServingSize),type(xServingSize),xServingSize)
+            ite=0
+            try:
+                xTxt=xServingSize[0].text
+                colname,ssize=xTxt.split('\n')
+            except:
+                colname="NA"
+                ssize="NA"
+            Myinfo['ServingSize']['colName']=str(colname)
+            Myinfo['ServingSize']['Size']=str(ssize)
 
-xServingSize=r.html.xpath('/html/body/div/main/div[2]/div/div[1]/div[1]')
-#print("xServingSize : len type and content ",len(xServingSize),type(xServingSize),xServingSize)
-ite=0
-xTxt=xServingSize[0].text
-colname,ssize=xTxt.split('\n')
-Myinfo['ServingSize']['colName']=str(colname)
-Myinfo['ServingSize']['Size']=str(ssize)
+            #Time of preparation
+            Myinfo['rTime']=[]
+            xRtime=r.html.xpath('//*[@id="wp--skip-link--target"]/div[2]/div/div[1]/div[2]')
+            #print("xRtime : len type and content ",len(xRtime),type(xRtime),xRtime[0])
+            for item in xRtime:
+                pieces=item.text.split('\n')
+                for piece in pieces:
+                    Myinfo['rTime'].append(piece)
 
-#Time of preparation
-Myinfo['rTime']=[]
-xRtime=r.html.xpath('//*[@id="wp--skip-link--target"]/div[2]/div/div[1]/div[2]')
-#print("xRtime : len type and content ",len(xRtime),type(xRtime),xRtime[0])
-for item in xRtime:
-    pieces=item.text.split('\n')
-    for piece in pieces:
-        Myinfo['rTime'].append(piece)
+            #Extracting VideoUrl
+            Myinfo['VideoUrl']={}
+            fVideoUrl=r.html.find('iframe')
+            if DEBUG: print("VIDEO URL IN YT ==>")
+            for xv in fVideoUrl:
+                if DEBUG:print(xv)
+                src=xv.attrs['src']
+                alt=xv.attrs['title']
+                Myinfo['VideoUrl']['src']=src
+                Myinfo['VideoUrl']['alt']=alt
+            #print("VIDEO URL IN YT ==>",VideoUrl.text)
+            #Myinfo['VideoUrl']=VideoUrl.text
 
-#Extracting VideoUrl
-Myinfo['VideoUrl']={}
-fVideoUrl=r.html.find('iframe')
-if DEBUG: print("VIDEO URL IN YT ==>")
-for xv in fVideoUrl:
-    if DEBUG:print(xv)
-    src=xv.attrs['src']
-    alt=xv.attrs['title']
-    Myinfo['VideoUrl']['src']=src
-    Myinfo['VideoUrl']['alt']=alt
-#print("VIDEO URL IN YT ==>",VideoUrl.text)
-#Myinfo['VideoUrl']=VideoUrl.text
-
-jSonPrint(Myinfo)
+            jSonPrint(Myinfo)
 
 with open (OUTPUTFILE,"w") as o:
     json.dump(allInfo,o)
