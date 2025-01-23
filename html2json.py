97efbb6756363de6d85e0cb94de9d08cbf767e0b
Ramon Castillo <ramon@rcastillo.net>
2025-01-23 22:36:52 +0100

    fixed the creation of a Dict of Dicts

--- html2json.py
+++ html2json.py
@@ -37,8 +37,6 @@ def h_name(url):
     hname=s_url[len(s_url)-2]
     return hname
 def jSonPrint(ThisInfo):
-    Header=ThisInfo['RecipeCodeName']
-    allInfo[Header]=ThisInfo
     fData=json.dumps(ThisInfo,indent=4)
     print("....json....")
     print(fData)
@@ -53,51 +51,64 @@ with open(LINKSTOPROCESS,'r') as Recipes_links:
             Header=h_name(url)
             jLine='"RecipeCodeName":'+'"'+Header+'"'
             Myinfo['RecipeCodeName']=Header
+            allInfo[Header]={}
+            allInfo[Header]['RecipeCodeName']=Header
             mPrint(jLine)
 
             ##### Getting the Session and Rendering the page
             s = HTMLSession()
             r = s.get(url)
 
-            r.html.render(sleep=1)
+            r.html.render(sleep=2)
 
             if DEBUG:print(r.status_code)
-
+            #Link
+            Myinfo["Link"]=url
+            allInfo[Header]["Link"]=url
             #ShortDescription=<title>
             ShortDescription=r.html.find("title" , first=True)
             jLine='"ShortDescription :"'+ShortDescription.text
             Myinfo['ShortDescription']=ShortDescription.text
+            allInfo[Header]['ShortDescription']=ShortDescription.text
             if DEBUG : mPrint(jLine)
 
             #Title 
             Title=r.html.xpath('/html/body/div/main/div[1]/h3',first=True)
             Myinfo['Title']=Title.text
+            allInfo[Header]['Title']=Title.text
 
             #Descrpition
             Description=r.html.xpath('/html/body/div/main/div[2]/p[1]',first=True)
             try:
                 Myinfo['Description']=Description.text
+                allInfo[Header]['Description']=Description.text
             except:
                 Myinfo['Description']="CHECK DESCRIPTION @"+url
+                allInfo[Header]['Description']="CHECK DESCRIPTION @"+url
 
             #Ingredients as a list
             Myinfo['Ingredients']=[]
+            allInfo[Header]['Ingredients']=[]
             xIngredients=r.html.xpath('/html/body/div/main/div[2]/div/div[2]/ul')
             for ulli in xIngredients:
                 if DEBUG: print(ulli.text)
-                Myinfo['Ingredients'].append(ulli.text)   
+                Myinfo['Ingredients'].append(ulli.text)
+                allInfo[Header]['Ingredients'].append(ulli.text)   
 
             #Directions as a list
             Myinfo['Directions']=[]
+            allInfo[Header]['Directions']=[]
             xDirections=r.html.xpath('/html/body/div/main/div[2]/div/div[3]')
 
             if DEBUG: print("Directions type ",type(xDirections),"and length",len(xDirections))
             for li in xDirections:
                 if DEBUG: print(li.text)
                 Myinfo['Directions'].append(li.text)
+                allInfo[Header]['Directions'].append(li.text)
 
             # Serving Size as a list
             Myinfo['ServingSize']={}
+            allInfo[Header]['ServingSize']={}
 
             xServingSize=r.html.xpath('/html/body/div/main/div[2]/div/div[1]/div[1]')
             #print("xServingSize : len type and content ",len(xServingSize),type(xServingSize),xServingSize)
@@ -109,19 +120,24 @@ with open(LINKSTOPROCESS,'r') as Recipes_links:
                 colname="NA"
                 ssize="NA"
             Myinfo['ServingSize']['colName']=str(colname)
+            allInfo[Header]['ServingSize']['colName']=str(colname)
             Myinfo['ServingSize']['Size']=str(ssize)
+            allInfo[Header]['ServingSize']['Size']=str(ssize)
 
             #Time of preparation
             Myinfo['rTime']=[]
+            allInfo[Header]['rTime']=[]
             xRtime=r.html.xpath('//*[@id="wp--skip-link--target"]/div[2]/div/div[1]/div[2]')
             #print("xRtime : len type and content ",len(xRtime),type(xRtime),xRtime[0])
             for item in xRtime:
                 pieces=item.text.split('\n')
                 for piece in pieces:
                     Myinfo['rTime'].append(piece)
+                    allInfo[Header]['rTime'].append(piece)
 
             #Extracting VideoUrl
             Myinfo['VideoUrl']={}
+            allInfo[Header]['VideoUrl']={}
             fVideoUrl=r.html.find('iframe')
             if DEBUG: print("VIDEO URL IN YT ==>")
             for xv in fVideoUrl:
@@ -129,10 +145,14 @@ with open(LINKSTOPROCESS,'r') as Recipes_links:
                 src=xv.attrs['src']
                 alt=xv.attrs['title']
                 Myinfo['VideoUrl']['src']=src
+                allInfo[Header]['VideoUrl']['src']=src
                 Myinfo['VideoUrl']['alt']=alt
+                allInfo[Header]['VideoUrl']['alt']=alt
             #print("VIDEO URL IN YT ==>",VideoUrl.text)
             #Myinfo['VideoUrl']=VideoUrl.text
+    
 
+            
             jSonPrint(Myinfo)
 
 with open (OUTPUTFILE,"w") as o:
